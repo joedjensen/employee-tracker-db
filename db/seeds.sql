c02dfd1536ff1b871a2b1b3682407c1fcbb4fb50
josephjensen <joe@cryptonumerics.com>
2022-11-01 22:24:41 -0700

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,25 @@
 INSERT INTO departments (department_name)
-VALUES ("Accounting"),
-       ("Sales"),
+VALUES ("Sales"),
        ('Engineering'),
-       ("Product"),
-       ("IT");
+       ("Finance"),
+       ("Legal");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Accountant", 125000.00, 1),
-       ("Sales person", 85000.00, 2),
-       ("Engineer", 150000.00, 3),
-       ("PM", 175000, 4),
-       ("Printer Guy", 45000.00, 5);
+VALUES ("Sales Lead", 100000.00, 1),
+       ("Salesperson", 80000.00, 1),
+       ("Lead Engineer", 150000.00, 2),
+       ("Software Engineer", 120000.00, 2),
+       ("Account Manager", 160000.00, 3),
+       ("Accountant", 125000.00, 3),
+       ("Legal Team Lead", 250000.00, 4),
+       ("Lawyer", 190000.00, 4);
 
 INSERT INTO employees(first_name, last_name, role_id, manager_id)
-VALUES ("Jerry", "Chenette", 1, NULL),
-       ("Manny","Nunes", 2, 1),
-       ("Monica","Holboke", 4, NULL),
-       ("Joe","Jensen", 3, 3),
-       ("Jacob", "Cloaca", 5, 3);
+VALUES ("John", "Doe", 1, NULL),
+       ("Mike","Chan", 2, 1),
+       ("Ashley","Rodriguez", 3, NULL),
+       ("Kevin","Tupik", 4, 3),
+       ("Kunal","Singh", 5, NULL),
+       ("Malia","Brown", 6, 5),
+       ("Sarah","Lourd", 7, NULL),
+       ("Tom","Allen", 8, 7);
